2948df0ca10a50db9460d1407e0e696b6262a718
Suthana-K_tcct <Suthana.K@tcc-technology.com>
2026-07-14 13:05:43 +0700

    I don't think I will add anything more for now

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,65 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void __print(int x) {cerr << x;}
-void __print(long x) {cerr << x;}
-void __print(long long x) {cerr << x;}
-void __print(unsigned x) {cerr << x;}
-void __print(unsigned long x) {cerr << x;}
-void __print(unsigned long long x) {cerr << x;}
-void __print(float x) {cerr << x;}
-void __print(double x) {cerr << x;}
-void __print(long double x) {cerr << x;}
-void __print(char x) {cerr << '\'' << x << '\'';}
-void __print(const char *x) {cerr << '\"' << x << '\"';}
-void __print(const string &x) {cerr << '\"' << x << '\"';}
-void __print(bool x) {cerr << (x ? "true" : "false");}
 
-template<typename T, typename V>
-void __print(const pair<T, V> &x);
-template<typename T>
-void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
-template<typename T, typename V>
-void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
-void _print() {cerr << "]\n";}
-template <typename T, typename... V>
-void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
-//#ifdef DEBUG
-#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
-//#else
-//#define dbg(x...)
-//#endif
+// --- User Defined Types & Shorthands ---
+using ll = long long;
+using ld = long double;
+using cd = complex<ld>;
 
-typedef long long ll;
-typedef long double ld;
-typedef complex<ld> cd;
- 
-typedef pair<int, int> pi;
-typedef pair<ll,ll> pl;
-typedef pair<ld,ld> pd;
- 
-typedef vector<int> vi;
-typedef vector<ld> vd;
-typedef vector<ll> vl;
-typedef vector<pi> vpi;
-typedef vector<pl> vpl;
-typedef vector<cd> vcd;
- 
-//template<class T> using pq = priority_queue<T>;
-//template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using pd = pair<ld, ld>;
 
-#define rep(i, a) for(int i=0;i<a;++i)
-#define FOR(i, a, b) for (int i=a; i<(b); i++)
-#define F0R(i, a) for (int i=0; i<(a); i++)
-#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
-#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
-#define trav(a,x) for (auto& a : x)
+using vi = vector<int>;
+using vd = vector<ld>;
+using vl = vector<ll>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vcd = vector<cd>;
+
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+// --- Macros & Utilities ---
+#define rep(i, a) for (int i = 0; i < a; ++i)
+#define FOR(i, a, b) for (int i = a; i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a, x) for (auto& a : x)
 #define uid(a, b) uniform_int_distribution<long long>(a, b)(rng)
- 
+
 #define sz(x) (int)(x).size()
 #define mp make_pair
 #define pb push_back
-//#define f first
-//#define s second
 #define lb lower_bound
 #define ub upper_bound
 #define all(x) x.begin(), x.end()
@@ -68,13 +40,16 @@ typedef vector<cd> vcd;
 template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
 template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
 
+// --- RNG Seed ---
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
+// --- Constants ---
 const char nl = '\n';
 const int N = 2e5+3;
+const int MOD = 998244353;
 const int INF = INT_MAX;
 const long long LINF = LLONG_MAX;
 
-int main(){
-    ios::sync_with_stdio(false);cin.tie(nullptr);
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
 }
